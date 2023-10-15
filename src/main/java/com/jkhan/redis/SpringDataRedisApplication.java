24633f80a451527be4f6dda7d5e1f3bc90f6c6b3
YourLord00 <jtang@fandm.edu>
2023-10-14 22:26:28 -0400

    redis data

---
+++ src/main/java/com/jkhan/redis/SpringDataRedisApplication.java
@@ -0,0 +1,44 @@
+package com.jkhan.redis;
+
+import com.jkhan.redis.entity.Product;
+import com.jkhan.redis.repository.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/product")
+public class SpringDataRedisApplication {
+
+	@Autowired
+	private ProductDao productDao;
+
+	@PostMapping
+	public Product save(@RequestBody Product product) {
+		return productDao.save(product);
+	}
+
+	@GetMapping
+	public List<Product> getAllProducts() {
+		return productDao.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Product findProduct(@PathVariable int id) {
+		return productDao.findProductById(id);
+	}
+
+	@DeleteMapping("{/id}")
+	public String  removeProduct(@PathVariable int id) {
+		return productDao.deleteProduct(id);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRedisApplication.class, args);
+	}
+
+}
